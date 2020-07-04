6653e47af514a8a791ea3678c0414cc12f821b3d
Inna Vasilyeva <vasilyeva_inna@ukr.net>
2020-07-04 14:56:34 +0300

    Cities

---
+++ SunriseSunset/Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using SunriseSunset.Models;
+using System.Data.Entity;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace SunriseSunset
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Database.SetInitializer(new DbInitializer());
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
---
+++ SunriseSunset/Models/Context.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SunriseSunset.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<City> Cities { get; set; }
+    }
+}
